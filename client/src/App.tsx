0f64c8f40521945d546057afa77cc2d700d99cad
psyformation <psy.formation.lhi@gmail.com>
2026-05-07 12:06:02 +0200

    add: client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Landing from "@/pages/landing";
+import MoodTracker from "@/pages/mood-tracker";
+import PatientMode from "@/pages/patient-mode";
+import ClinicalEvaluation from "@/pages/clinical-evaluation";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Landing} />
+      <Route path="/patient" component={PatientMode} />
+      <Route path="/praticien" component={MoodTracker} />
+      <Route path="/clinical" component={ClinicalEvaluation} />
+      <Route component={Landing} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
